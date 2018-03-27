290478c20345231798f29d30ce2ffc643a3cbbcd
SharukSingh <sharuksingh@outlook.com>
2018-03-27 19:09:15 +0545

    Added new character in welcome page

--- application/views/welcome_page.php
+++ application/views/welcome_page.php
@@ -2,12 +2,10 @@
      
             <div class="content">
                 <div class="p-3">
-                    <img src=<?php echo base_url()."assets/images/Teacher_Vectors-5.png"?> alt="Teacher" class="img-fluid">
+                    <img src=<?php echo base_url()."assets/images/swagat.png"?> alt="Teacher" class="img-fluid">
                 </div>
 
-                <h2>welcome...</h2>
-
-                <div class="text-center">
+                <div class="text-center p-3">
                     <button type="button" class="btn btn-primary btn-lg mx-3">Take a lesson</button>
                     <button type="button" class="btn btn-secondary btn-lg mx-3">Take a test</button>
                 </div>
