deab84bc9954259d1c926605e484e289ac91a6d6
Sushil <ssharma@growbydata.com>
2018-04-19 21:51:32 +0545

    Welcome page/lessons content changes

--- application/views/lesson.php
+++ application/views/lesson.php
@@ -1,4 +1,7 @@
-<?php include_once("header.php"); ?>
+<?php 
+    include_once("header.php");
+    $chapter= $this->input->get('chapter');
+ ?>
      
             <div class="content p-3">
                 <h2>Lesson title</h2>
@@ -8,14 +11,14 @@
                         <source src="https://www.youtube.com/watch?v=hzYjE0qJJTY" type="video/mp4">
                         Your browser does not support the video tag.
                     </video> -->
-                    <iframe width="320" height="240" src="https://www.youtube.com/embed/yAvPS6RsEBU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                    <iframe width="320" height="240" src="https://www.youtube.com/embed/egt57g5VXq8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 </div>
 
                 <p class="lead">Short description about the video/lesson</p>
 
                 <div class="options mt-5">
-                    <button type="button" class="btn btn-info btn-lg">Example Solutions</button>
-                    <button type="button" class="btn btn-info btn-lg">Take a test</button>
+                    <!-- <button type="button" class="btn btn-info btn-lg">Example Solutions</button> -->
+                    <a href="<?php echo base_url().'page/test?chapter='.$chapter?>"><button type="button" class="btn btn-info btn-lg">Take a test</button></a>
                 </div>
             </div>
 
--- application/views/welcome_page.php
+++ application/views/welcome_page.php
@@ -23,8 +23,8 @@
                 </div>
 
                 <div class="text-center p-3">
-                    <button type="button" class="btn btn-primary btn-lg mx-3">Take a lesson</button>
-                    <button type="button" class="btn btn-secondary btn-lg mx-3">Take a test</button>
+                    <a href="<?php echo base_url().'page/lessons'?>"><button type="button" class="btn btn-primary btn-lg mx-3">Take a lesson</button></a>
+                    <!-- <button type="button" class="btn btn-secondary btn-lg mx-3">Take a test</button> -->
                 </div>
             </div>
 
