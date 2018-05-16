1014c600f83a96335af2157be7029d6241563771
SharukSingh <sharuksingh@outlook.com>
2018-05-16 08:41:43 +0545

    changed the thumbnail:

--- application/views/lessons.php
+++ application/views/lessons.php
@@ -16,8 +16,8 @@
                     foreach ($query->result() as $row){
                     echo '<div class="content p-3 chapters">
                               <div class="row lesson my-3">
-                                <div class="col-xs-4 col-md-4 col-lg-3">
-                                <img src= "'.base_url().'assets/images/hqdefault.jpg" alt="thumbnail" class="img-fluid">
+                                <div class="col-xs-4 col-md-4 col-lg-3 text-center">
+                                <img src= "'.base_url().'assets/images/book-icon.png" alt="thumbnail" class="img-fluid ml-3 mb-xs-3" width="50%">
                               </div>
                               <div class="col-xs-8 col-md-8 col-lg-9">
                                 <h3><a href="'.base_url().'page/lesson?chapter='.$row->chapter_number.'">'.$row->chapter_name.'</a></h3>
