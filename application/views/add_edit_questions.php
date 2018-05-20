5c03f7fa735c9409a42916ab4bd1a91b1eefb99b
SharukSingh <sharuksingh@outlook.com>
2018-05-20 09:09:46 +0545

    styled question box

--- application/views/add_edit_questions.php
+++ application/views/add_edit_questions.php
@@ -53,7 +53,7 @@
                         <form id = "form-<?php echo $question_id ?>" action="../ajax/edit_questions" method="post">
                         	<input type="hidden" name="id" value = "<?php echo $question_id ?>">
                         	<input type="hidden" name="chapter" value = "<?php echo $row->chapter ?>">
-	                        <p><textarea type="text" name="questions" rows="3" cols="120"><?php echo $row->question?></textarea></p>
+	                        <p><textarea type="text" name="questions" rows="3" cols="120" class="question-field"><?php echo $row->question?></textarea></p>
 	                        <p><b>Question Image: </b>
 	                        	<input type="file" name="question-image-<?php echo $question_id ?>[]" id="question-image-<?php echo $question_id ?>-1" multiple>
 	                        </p>
